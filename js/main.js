a9cb6b1fdee79a7f513d81c8b83481401335a6a8
Магомед <magomed@iMac-Color-IT.local>
2017-09-11 11:34:48 +0300

    make adaptive for tabs, make mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var DESKTOP_MODE = window.innerWidth > 1300;
+var MOBILE_MODE = window.innerWidth <= 1300;
+
+var body = document.querySelector('body');
 var wellBlock = document.querySelector('.well-illustration');
 var tubeBlock = document.querySelector('.tube-block');
 var interactiveBlocs = document.querySelectorAll('.interactive-block');
@@ -9,6 +13,11 @@ var sliderMainSlides = document.querySelectorAll('.slider-main__slide');
 var infographicImgWrap = document.querySelector('.infographic-img-wrap');
 var aboutCompanyThumbs = document.querySelector('.about-company-thumbs');
 var aboutCompanyMainImage = document.querySelector('.about-company-main-photo img');
+var mobileMenuBlock = document.querySelector('.main-menu-block');
+var showMobileMenuBtn = document.querySelector('.hamburger');
+var hideMobileMenuBtn = document.querySelector('.main-menu-close-mobile');
+var mobileMenuHeight = parseInt(getComputedStyle(mobileMenuBlock).height);
+
 
 var infographicAnimate = function() {
     var pageHeight = document.documentElement.clientHeight;
@@ -72,7 +81,7 @@ window.addEventListener('scroll', infographicAnimate);
 
 tubeAnimatedBlock.addEventListener('transitionend', ventileSpin);
 
-aboutCompanyThumbs.addEventListener('click', pickPhoto)
+DESKTOP_MODE && aboutCompanyThumbs.addEventListener('click', pickPhoto)
 
 var certificatesSlider = document.querySelector('.certificates-slider');
 if(certificatesSlider) {
@@ -82,6 +91,23 @@ if(certificatesSlider) {
       wrapAround: true,
       autoPlay: true,
       pageDots: false,
+      groupCells: 1,
+      arrowShape: { 
+        x0: 15,
+        x1: 55, y1: 50,
+        x2: 60, y2: 45,
+        x3: 25
+      }
+    });
+}
+
+var aboutCompanySlider = document.querySelector('.about-company-thumbs');
+if(MOBILE_MODE && aboutCompanySlider) {
+    var aboutCompanySliderFlkty = new Flickity( aboutCompanySlider, {
+      // options
+      cellAlign: 'left',
+      wrapAround: true,
+      autoPlay: true,
       groupCells: 1,
       arrowShape: { 
         x0: 15,
@@ -113,4 +139,29 @@ var mainSliderActivate = function(e) {
     })
 }
 
-sliderMainTabs.addEventListener('click', mainSliderActivate )
+sliderMainTabs.addEventListener('click', mainSliderActivate );
+
+var fixBody = function() {
+    body.style.height = mobileMenuHeight - 10 + 'px';
+    body.style.overflow = 'hidden'
+}
+
+var unfixBody = function() {
+    body.style.height = '';
+    body.style.overflow = ''
+}
+
+var showMobileMenu = function(e) {
+    e.preventDefault();
+    mobileMenuBlock.classList.remove('closed');
+    fixBody();
+}
+
+var hideMobileMenu = function(e) {
+    e.preventDefault();
+    mobileMenuBlock.classList.add('closed');
+    unfixBody();
+}
+
+showMobileMenuBtn.addEventListener('click', showMobileMenu);
+hideMobileMenuBtn.addEventListener('click', hideMobileMenu);
